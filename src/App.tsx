5aab0b08d96a4b9c2e5835dfa2b2239827b28d5c
= <=>
2024-05-08 20:56:41 +0530

    added dark theam and made it responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import './App.css'
 import Card from './card';
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
+import { ModeToggle } from '@/components/mode-toggle'
 import { useState } from 'react';
+
+import { ThemeProvider } from "@/components/theme-provider"
+
+import  { useEffect } from 'react';
+
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
@@ -12,10 +18,22 @@ function App() {
 
   const handleSearch = async () => {
     const response = await fetch('https://musica-api-8odu.onrender.com/search/'+searchValue);
+    console.log(searchValue);
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
     const data = await response.json();
+    setSongs(data);
+
+  };
+  const defaultSearch = async () => {
+    console.log("hello")
+    const response = await fetch('https://musica-api-8odu.onrender.com/search/mai');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
     setSongs(data);
 
   };
@@ -25,25 +43,34 @@ function App() {
     setSearchValue(event.target.value);
     
   };
+  useEffect(() => {
+    defaultSearch();
+  }, []);
 
   return (
     <>
-    {/* <div className='flex bg-slate-700 px-5'>
-      <h1 className='text-white text-2xl py-3'>Musica</h1>
-    </div> */}
-      <div className="flex p-5">
-        <Input placeholder='Search' value={searchValue} onSubmit={handleSearch} onChange={handleInputChange} />
-        <Button className='px-8' onClick={handleSearch}>Search</Button>
-      </div>
-      <section className='flex items-center flex-col'>
-        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {/* <div className="flex flex-col items-center"> */}
-          {songs.map((song, index) => (
-            <Card  key={index} {...song} />
-          ))}
+      <section className='flex items-center flex-col bg-slate-800'>
 
+      <div className="flex p-5 bg-gray-900 width w-full lg:px-48">
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <ModeToggle/>
+        </ThemeProvider>
+        <Input className='mx-1' placeholder='Search' value={searchValue} onSubmit={handleSearch} onChange={handleInputChange} />
+        <Button className='px-8 border' onClick={handleSearch}>Search</Button>
+      </div>
+      <div className=" lg:px-44">
+        <div className="lg:grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {/* <div className="flex flex-col items-center"> */}
+            {songs.map((song, index) => (
+              <Card  key={index} {...song} />
+            ))}
+          </div>
         </div>
       </section>
+      <footer className="bg-gray-800 text-white text-center py-4">
+        <p>Designed by Manik Sharma</p>
+        <p className="pt-3">&copy; 2024 Manik Sharma. All rights reserved.</p>
+      </footer>
     </>
   )
 }
--- src/card.tsx
+++ src/card.tsx
@@ -9,11 +9,11 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ song_name, artists_name, song_icon, download_link }) => {
     return (
-        <div className="card border-2 border-gray-300 rounded-lg p-4 m-4 w-64 hover:scale-110 transition-transform duration-300">
+        <div className="text-white bg-gray-900 card border-gray-300 rounded-lg p-4 m-4 w-64 hover:scale-110 transition-transform duration-300">
             <img src={song_icon} alt={song_name} className="w-full h-auto rounded-lg" />
             <h3 className="mt-2 text-lg">{song_name}</h3>
             <p className="mb-2 text-sm">Artist : {artists_name}</p>
-            <a href={download_link} download className="block text-center bg-green-700 text-white py-1 px-2 rounded-md no-underline">
+            <a href={download_link} download className="block text-center bg-green-600 text-white py-1 px-2 rounded-md no-underline">
                 Download
             </a>
         </div>
