767d0a4826b99047ab62c48a740a44a74a4b2337
= <=>
2024-05-09 13:29:53 +0530

    made download button stay at bottom

--- src/card.tsx
+++ src/card.tsx
@@ -9,10 +9,11 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ song_name, artists_name, song_icon, download_link }) => {
     return (
-        <div className="text-white border bg-gray-900 card border-gray-600 rounded-lg p-4 m-4 w-64 hover:scale-110 transition-transform duration-300">
+        <div className="text-white border bg-gray-900 card border-gray-600 rounded-lg p-4 m-4 w-64 hover:scale-110 transition-transform duration-300 flex flex-col">
             <img src={song_icon} alt={song_name} className="w-full h-auto rounded-lg" />
             <h3 className="mt-2 text-lg">{song_name}</h3>
             <p className="mb-2 text-sm">Artist : {artists_name}</p>
+            <div className="flex-grow"></div>
             <a href={download_link} download className="block text-center bg-green-600 text-white py-1 px-2 rounded-md no-underline">
                 Download
             </a>
