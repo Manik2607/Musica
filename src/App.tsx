d2ad7dd42d4fe833acd8eb64718d17be28da1bbd
= <=>
2024-07-09 13:20:36 +0530

    Added option to do bulk download

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import Card from './card';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './components/ui/sheet'
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import { ModeToggle } from '@/components/mode-toggle'
 import { useState } from 'react';
-
 import { ThemeProvider } from "@/components/theme-provider"
-
 import  { useEffect } from 'react';
+
 
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
 
   const [songs, setSongs] = useState<any[]>([]);
+
+  const [bulkSongs, setbulkSongs] = useState("");
 
 
   const handleSearch = async () => {
@@ -24,6 +26,22 @@ function App() {
     }
     const data = await response.json();
     setSongs(data);
+
+  };
+  const handleBulkDownload = async () => {
+    const songList = bulkSongs.split("\n");
+    console.log(songList);
+    for (let i = 0; i < songList.length; i++) {
+      const response = await fetch('https://musica-api-8odu.onrender.com/getSong/'+songList[i]);
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const downloadLink = await response.json();
+      const downloadLinkElement = document.createElement('a');
+      downloadLinkElement.href = downloadLink;
+      downloadLinkElement.download = 'song.mp3';
+      downloadLinkElement.click();
+    }
 
   };
   const defaultSearch = async () => {
@@ -58,6 +76,18 @@ function App() {
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <ModeToggle/>
         </ThemeProvider>
+        <Sheet>
+        <SheetTrigger>
+          <Button className='px-4 border border-gray-600'>Bulk</Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Bulk Download</SheetTitle>
+          </SheetHeader>
+            <textarea className='w-full h-64 mt-5 p-2 border border-gray-600' placeholder='Song names..' value={bulkSongs} onChange={(event)=>setbulkSongs(event.target.value)} ></textarea>
+            <Button className='px-8 border border-gray-600' onClick={handleBulkDownload}>Download</Button>
+        </SheetContent>
+      </Sheet>
         <Input onKeyDown={handleKeyDown}  className='mx-1' placeholder='Search' value={searchValue} onSubmit={handleSearch} onChange={handleInputChange} />
         <Button className='px-8 border border-gray-600' onClick={handleSearch}>Search</Button>
       </div>
